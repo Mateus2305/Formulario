20c5710122834f3e2072eb7fed5eba3e84ba9b38
Fortelo Vendas <90217585+Mateus2305@users.noreply.github.com>
2022-08-17 08:54:21 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,64 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const telefone = document.getElementById("telefone");
+
+
+form.addEventListener("submit", (e) =>{
+    e.preventDefault();
+
+    checkInputs();
+})
+
+function checkInputs(){
+  const usernameValue = username.value;
+  const emailValue = username.value;
+  const telefoneValue = telefone.value;
+
+if(usernameValue === ""){
+    setErrorFor(username, "Campo obrigátorio.");
+  } else {
+    setSucessFor(username);
+  }
+
+  if(emailValue === ""){
+    setErrorFor(email, "Campo obrigátorio.");
+} else if (!checkEmail(emailValue)) {
+  setErrorFor(email,"Por favor insira um email válido.");
+} else {
+  setSucessFor(email);
+  }
+  if(telefoneValue === ""){
+    setErrorFor(telefone, "Campo obrigátorio.");
+} else if (!checkTelefone(telefoneValue)) {
+  setErrorFor(telefone,"Por favor insira um telefone válido.");
+} else {
+  setSucessFor(telefone);
+  }
+}
+
+function setErrorFor(input, message){
+  const formControl = input.parentElement;
+  const small = formControl.querySelector("small");
+
+  //Adicionar a mensagem de setErrorFor
+    small.innerText = message;
+  //Adiciona classe de erro
+    formControl.className = "form-control error";
+}
+function setSucessFor(input){
+  const formControl = input.parentElement
+
+  // Adicionar a classe  de sucesso
+  formControl.className = "form-control sucess";
+}
+function checkEmail(email) {
+  return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    email
+  );
+}
+function checkTelefone(telefone){
+  return /^\\([0-9]{2}\\)((3[0-9]{3}-[0-9]{4})|(9[0-9]{3}-[0-9]{5}))$/.test(
+    telefone
+  );
+}
